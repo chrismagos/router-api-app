68036c6f41af474f52f1b0c58c6657f4249b1f57
ChrisMagos <chris.magos07@gmail.com>
2024-06-01 16:04:44 +0200

    Added albums display

--- src/Components/Artist.jsx
+++ src/Components/Artist.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import getArtist from "../Services/getArtist";
+import getAlbums from "../Services/getAlbums";
 
 const Artist = () => {
     const { id: artistId } = useParams();
     const [artist, setArtist] = useState(null);
     const token = window.localStorage.getItem("token");
+    const [albums, setAlbums] = useState(null)
 
     const fetchArtist = async () => {
         try {
@@ -17,10 +19,28 @@ const Artist = () => {
         }
     };
 
+    const fetchAlbums = async () => {
+        try {
+            const data = await getAlbums(token, artistId);
+            setAlbums(data);
+        } catch (error) {
+            console.error('Error fetching albums data:', error);
+        }
+    }
+
     if (!artist) {
         fetchArtist();
         return <div>Loading...</div>;
     }
+
+    if(artist && !albums){
+        fetchAlbums();
+        return <div>Loading...</div>;
+    }
+
+    const albumsOfType = (type) => {
+        return albums.filter(album => album.album_type === type);
+    };
 
     return (
         <div className="artist-details">
@@ -28,6 +48,31 @@ const Artist = () => {
             {artist.images.length && <img width="40%" src={artist.images[0].url} alt={artist.name} />}
             <p>Followers: {artist.followers.total}</p>
             <p>Genres: {artist.genres.join(", ")}</p>
+            <h2>Releases</h2>
+            <div>
+                <h3>Albums</h3>
+                    <ul>
+                        {albumsOfType("album").map(album => (
+                            <li key={album.id}>{album.name}</li>
+                        ))}
+                    </ul>
+            </div>
+            <div>
+                <h3>Singles</h3>
+                <ul>
+                    {albumsOfType("single").map(single => (
+                        <li key={single.id}>{single.name}</li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h3>EPs</h3>
+                <ul>
+                    {albumsOfType("album").map(ep => (
+                        <li key={ep.id}>{ep.name}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
--- src/Services/getAlbums.js
+++ src/Services/getAlbums.js
@@ -1,4 +1,18 @@
-const getAlbums = () => {
+const getAlbums = async (token, artistId) => {
+    try {
+        const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Network response was not ok`);
+        }
+        const data = await response.json();
+        return data.items;
+    } catch (error) {
+        console.error('Error fetching album data:', error);
+    }
+};
 
-}
 export default getAlbums;
