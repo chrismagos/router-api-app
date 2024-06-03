b6ae3e26e77ad6202ec723fff7e5c097d5b5e998
ChrisMagos <chris.magos07@gmail.com>
2024-06-03 17:50:12 +0200

    Text change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,10 +2,10 @@ const Home = () => {
     return <>
         <h1>Home</h1>
         <p>
-        Welcome to our music discovery platform! Explore a vast collection of artists, albums, and tracks, all integrated seamlessly with Spotify. Use our search feature to find your favorite artists, view detailed album information, and stay updated with the latest releases. Enjoy a rich musical experience tailored to your tastes. Dive in and start your musical journey with us today!
+            Welcome to my music discovery platform! Here, you can explore a wide range of artists, albums, and tracks, all integrated with Spotify. Use the search feature to find your favorite artists, get detailed album information, and stay updated with the latest releases.
         </p>
         <p>
-            For future updates visit our <a href="https://github.com/chrismagos/router-api-app">GitHub repository</a>.
+            For future updates visit my <a href="https://github.com/chrismagos/router-api-app">GitHub repository</a>.
         </p>
     </>
 }
